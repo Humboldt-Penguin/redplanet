a36ce93c2fc7dad60e338173d6a7d598cc824bb1
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2024-09-23 23:04:51 -0400

    [Aesthetic] Rename tests.

--- tests/test_user_config.py
+++ tests/test_user_config.py
@@ -9,35 +9,35 @@ from redplanet.user_config import set_dirpath_datacache, get_dirpath_datacache
 
 
 ## Default data cache path
-def test__get_dirpath_datacache__with_default_value():
+def test__get_dirpath_datacache__default_value():
 	current_path = get_dirpath_datacache()
 	print(f'\n\t- Default data cache path: {current_path}')
 	assert current_path == Path(user_cache_dir(appname="redplanet")).resolve()
 
 
 ## Valid input: string
-def test__set_dirpath_datacache__with_valid_string():
+def test__set_dirpath_datacache__valid_string():
 	valid_path_str = "/tmp/redplanet_cache"
 	set_dirpath_datacache(valid_path_str)
 	assert get_dirpath_datacache() == Path(valid_path_str)
 
 
 ## Valid input: pathlib.Path
-def test__set_dirpath_datacache__with_valid_path_object():
+def test__set_dirpath_datacache__valid_path_object():
 	valid_path_obj = Path("/tmp/redplanet_cache")
 	set_dirpath_datacache(valid_path_obj)
 	assert get_dirpath_datacache() == valid_path_obj
 
 
 ## Invalid input: bad string/path
-def test__set_dirpath_datacache__with_invalid_string():
+def test__set_dirpath_datacache__invalid_string():
 	invalid_path_str = "\0"
 	with pytest.raises(ValueError):
 		set_dirpath_datacache(invalid_path_str)
 
 
 ## Invalid input: not string/path
-def test__set_dirpath_datacache__with_invalid_type():
+def test__set_dirpath_datacache__invalid_type():
 	invalid_path_type = 12345
 	with pytest.raises(TypeError):
 		set_dirpath_datacache(invalid_path_type)
