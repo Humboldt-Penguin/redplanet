e3eab05b515094b69d9bb97f35abcd407cd90b59
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2024-10-11 19:38:46 -0400

    [Tests] Fixed `tests/confest.py` so properly reset user cache dir value between tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,11 @@
 import pytest
 
-from redplanet.user_config import _dirpath_datacache, _enable_stream_hash_check
+import redplanet.user_config as uc
 
 @pytest.fixture(autouse=True)
 def reset_datacache_dir():
     """
     Reset values in `redplanet/user_config.py` before every test.
     """
-    global _dirpath_datacache, _enable_stream_hash_check
-    _dirpath_datacache = None
-    _enable_stream_hash_check = True
-
-
-# from redplanet.user_config import set_dirpath_datacache_default
-# @pytest.fixture(autouse=True)
-# def reset_datacache_dir():
-#     """
-#     Reset the data cache directory to the default location before each test.
-#     """
-#     set_dirpath_datacache_default()
+    uc._dirpath_datacache = None
+    uc._enable_stream_hash_check = True
