b4544f29d634c052a52cbd733c1b903c734ede36
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2024-11-15 21:25:01 -0500

    [Feature (minor)] Added `return_exact_coords` options to getters for GRS & moho; misc cleanup -- TODO test if I broke anything lol, tests are still severely lacking.

--- src/redplanet/Crust/moho/access.py
+++ src/redplanet/Crust/moho/access.py
@@ -1,3 +1,5 @@
+import copy
+
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -22,27 +24,31 @@ def get_registry() -> pd.DataFrame:
 
 def get_model_info() -> dict[ str, str|int ] | None:
     if _model_info is not None:
-        return _model_info.copy()
+        return copy.deepcopy(_model_info)
     return None
 
 
 
 def get(
-    lon        : float | list | np.ndarray,
-    lat        : float | list | np.ndarray,
-    interpolate: bool = False,
-    as_xarray  : bool = False,
-) -> float | np.ndarray | xr.Dataset:
-
-    dat_moho = _get_dataset()
+    lon : float | list | np.ndarray,
+    lat : float | list | np.ndarray,
+    interpolate         : bool = False,
+    as_xarray           : bool = False,
+    return_exact_coords : bool = False,
+) -> float | np.ndarray | list[np.ndarray, np.ndarray, np.ndarray] | xr.Dataset:
 
     ## input validation
-    # lon = np.array(lon)   # skip conversion since `interp` doesn't work with singleton array indices, for some reason
-    # lat = np.array(lat)
+    if as_xarray and return_exact_coords:
+        raise ValueError("Can't return both xarray and exact coordinates. Choose one.")
+
     _verify_coords(lon, lat)
     lon = _slon2plon(lon)
+    # lon = np.array(lon)   # skip conversion since `interp` doesn't work with singleton array indices, for some reason -- TODO: is the solution to use `np.atleast_1d`??? idk when i made this change unfortunately, dig through git history
+    # lat = np.array(lat)
+
 
     ## get data
+    dat_moho = _get_dataset()
     if interpolate:
         data = dat_moho.interp(
             lon = lon,
@@ -57,9 +63,17 @@ def get(
             method = 'nearest'
         )
 
-    if not as_xarray:
-        data = data.values
-        ## convert singleton numpy array to scalar
-        if data.shape == (): data = data.item()
 
+    ## Case 1: return type `xarray.Dataset`
+    if as_xarray:
+        return data
+
+    ## Case 2: return type `list[np.ndarray, np.ndarray, np.ndarray]` (data with coords)
+    if return_exact_coords:
+        return data.values, data.lon.values, data.lat.values
+
+    ## Case 3: return type `float` or `np.ndarray` (just data)
+    data = data.values
+    ## convert singleton numpy array to scalar
+    if data.shape == (): data = data.item()
     return data
--- src/redplanet/GRS/access.py
+++ src/redplanet/GRS/access.py
@@ -7,27 +7,34 @@ from redplanet.GRS.load import _get_dataset
 
 
 def get(
-    element  : str,
-    lon      : float,
-    lat      : float,
-    quantity : str  = 'concentration',
-    normalize: bool = False,
-    as_xarray: bool = False,
-) -> float | np.ndarray | xr.Dataset:
+    element : str,
+    lon     : float | np.ndarray,  ## TODO: test array inputs!!!
+    lat     : float | np.ndarray,
+    quantity           : str  = 'concentration',
+    normalize          : bool = False,
+    as_xarray          : bool = False,
+    return_exact_coords: bool = False,
+) -> float | np.ndarray | list[np.ndarray, np.ndarray, np.ndarray] | xr.Dataset:
 
-    dat_grs = _get_dataset()
 
     ## input validation
+    if as_xarray and return_exact_coords:
+        raise ValueError("Can't return both xarray and exact coordinates. Choose one.")
+
     if element not in ['al','ca','cl','fe','h2o','k','si','s','th']:
         raise ValueError(f"Element {element} is not in list of supported elements: ['al','ca','cl','fe','h2o','k','si','s','th'].")
 
-    lon = np.array(lon)
-    lat = np.array(lat)
+    if normalize and (element in ['cl','h2o','s']):
+        raise ValueError(f"Can't normalize a volatile element ('{element}') to a volatile-free (cl, h2o, s) basis.")
+
     _verify_coords(lon, lat)
     lon = _plon2slon(lon)
+    lon = np.atleast_1d(lon)
+    lat = np.atleast_1d(lat)
 
 
     ## get data
+    dat_grs = _get_dataset()
     data = (
         dat_grs
             .sel(element=element)
@@ -36,8 +43,6 @@ def get(
     )
 
     if normalize:
-        if element in ['cl','h2o','s']:
-            raise ValueError(f"Can't normalize a volatile element ('{element}') to a volatile-free (cl, h2o, s) basis.")
         volatiles = (
             dat_grs
                 .sel(element=['cl','h2o','s'])
@@ -47,9 +52,17 @@ def get(
         )
         data = data / (1 - volatiles)
 
-    if not as_xarray:
-        data = data.values
-        ## convert singleton numpy array to scalar
-        if data.shape == (): data = data.item()
 
+    ## Case 1: return type `xarray.Dataset`
+    if as_xarray:
+        return data
+
+    ## Case 2: return type `list[np.ndarray, np.ndarray, np.ndarray]` (data with coords)
+    if return_exact_coords:
+        return data.values, data.lon.values, data.lat.values
+
+    ## Case 3: return type `float` or `np.ndarray` (just data)
+    data = data.values
+    ## convert singleton numpy array to scalar
+    if data.shape == (): data = data.item()
     return data
