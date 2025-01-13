2b64fa42419d5b9c6e4b75f128b944d08e3ba749
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2025-01-12 19:23:10 -0500

    [Minor] Crater ages are now given as tuple of 3 floats, instead of str separated by ;

--- src/redplanet/Craters/load_access.py
+++ src/redplanet/Craters/load_access.py
@@ -25,8 +25,24 @@ def _load() -> None:
     fpath_df = _get_fpath_dataset('crater_db')
     global _df_craters
     _df_craters = pd.read_csv(fpath_df)
+
     _df_craters['plon'] = _slon2plon(_df_craters['lon'])
     _df_craters = _df_craters.replace(np.nan, None)
+
+    ## convert ages (e.g. '4.00;-0.08;0.05') to a list of floats
+    columns_to_convert = [col for col in _df_craters.columns if col.startswith('N_') or col.endswith('Age')]
+    def convert(val):
+        if isinstance(val, str):
+            parts = val.split(';')
+            if len(parts) == 3:
+                try:
+                    return [float(part) for part in parts]
+                except ValueError:
+                    return None
+        return None
+    for col in columns_to_convert:
+        _df_craters[col] = _df_craters[col].apply(convert)
+
     return
 
 
