da10b5d6b29c0a89c358313a927ce30568e34617
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2025-02-01 15:41:46 -0500

    [minor]

--- src/redplanet/Craters/__init__.py
+++ src/redplanet/Craters/__init__.py
@@ -2,7 +2,7 @@ __all__ = []
 
 
 
-from redplanet.Craters.load_access import get, get_dataset
+from redplanet.Craters.load_get import get, get_dataset
 __all__.extend([
     'get',
     'get_dataset',
