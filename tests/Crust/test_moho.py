1e38ddd913da077d614044e7e955b2598a5d393d
Zain Eris Kamal <zain.eris.kamal@rutgers.edu>
2025-03-01 20:46:52 -0500

    test(Crust): add/complete tests for all Crust modules (topo, boug, crust, dichotomy).
    NOTE: we don't test load/access for topo since that might lead to downloading 12 GB if done with something like github actions (?).
    
    TODO: tests for Mag (sh + depth).

--- tests/Crust/test_moho.py
+++ tests/Crust/test_moho.py
@@ -3,37 +3,75 @@ import numpy as np
 
 from redplanet import Crust
 from redplanet.DatasetManager.dataset_info import MohoDatasetNotFoundError
-from redplanet.helper_functions.coordinates import CoordinateError
 
 
-def test_invalid_interior_model():
-    args = {
-        'interior_model'   : 'meow',
-        'insight_thickness': 40,
-        'rho_south'        : 2900,
-        'rho_north'        : 2900,
-    }
 
+def test_load_invalid():
+
+    ## invalid interior (`ValueError`)
     with pytest.raises(ValueError, match='Unknown interior model'):
-        Crust.moho.load(**args)
+        Crust.moho.load(
+            interior_model    = 'meow',
+            insight_thickness = 40,
+            rho_south         = 2900,
+            rho_north         = 2900,
+        )
 
-
-def test_model_not_found():
-    args = {
-        'interior_model'   : 'Khan2022',
-        'insight_thickness': 0,
-        'rho_south'        : 0,
-        'rho_north'        : 0,
-    }
-
-    ## default behavior: raise custom error `MohoDatasetNotFoundError`
+    ## valid interior, but model not found -- 1/2: custom error `MohoDatasetNotFoundError`
     with pytest.raises(MohoDatasetNotFoundError):
-        Crust.moho.load(**args)
+        Crust.moho.load(
+            interior_model    = 'Khan2022',
+            insight_thickness = 0,
+            rho_south         = 0,
+            rho_north         = 0,
+        )
 
-    ## fail silently (returns False)
-    found = Crust.moho.load(**args, fail_silently=True)
+    ## valid interior, but model not found -- 2/2: fail silently
+    found = Crust.moho.load(
+        interior_model    = 'Khan2022',
+        insight_thickness = 0,
+        rho_south         = 0,
+        rho_north         = 0,
+        fail_silently     = True,
+    )
     assert (not found)
 
 
-# TODO (too lazy teehee)
-# def test_access():
+
+def test_registry():
+    df = Crust.moho.get_registry()
+    assert df.shape[0] == 21894
+
+
+
+def test_load_get_valid():
+    ## verify that moho spherical harmonic coefficients can be downloaded, loaded, accessed (plain value and crustal thickness), and switched between models
+    lons = np.linspace(-180, 360, 100)
+    lats = np.linspace(-90, 90, 100)
+
+    Crust.moho.load(
+        interior_model = 'Khan2022',
+        insight_thickness = 39,
+        rho_south = 2700,
+        rho_north = 2700,
+    )
+    assert np.allclose( Crust.moho.get(lons, lats).mean()               , 3335011.619244110 )
+    assert np.allclose( Crust.moho.get(lons, lats, crthick=True).mean() , 51386.64291032735 )
+
+    Crust.moho.load(
+        interior_model = 'Khan2022',
+        insight_thickness = 41,
+        rho_south = 2900,
+        rho_north = 2900,
+    )
+    assert np.allclose( Crust.moho.get(lons, lats).mean()               , 3327826.760446502 )
+    assert np.allclose( Crust.moho.get(lons, lats, crthick=True).mean() , 58571.50170793515 )
+
+
+    ## wraparound
+    lons = np.linspace(-180, 0, 100)
+    lats = np.array([-60, 0, 60])
+    assert np.allclose(
+        Crust.moho.get(lons    , lats),
+        Crust.moho.get(lons+360, lats)
+    )
--- tests/Crust/test_topo.py
+++ tests/Crust/test_topo.py
@@ -1,14 +1,11 @@
 import pytest
-import numpy as np
 
 from redplanet import Crust
-from redplanet.helper_functions.coordinates import CoordinateError
 
 
-def test_bad_model():
+def test_load_invalid():
+    ## invalid model
     with pytest.raises(ValueError, match='Invalid'):
         Crust.topo.load('meow')
 
-# TODO (too lazy teehee)
-# def test_access():
-# def test_unloaded():
+# not testing load/get since the datasets are huge
